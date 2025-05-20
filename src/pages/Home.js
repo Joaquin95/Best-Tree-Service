4bd3120970682e542dded2a7936d6e91c5ceb995
Joaquin Morales <mintinvestments95@gmail.com>
2025-05-20 17:42:07 -0500

    styling with tailwing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Home() {
     return (
-        <section className="bg-green-100 py-20 text-center">
-            <h1 className="text-4xl font-bold mb-4"> Best Tree Service</h1>
-            <p className="text-lg">Professional Tree Trimming & Removal in Your Area </p>
+        <section className="bg-beige py-20 text-center">
+            <h1 className="text-5xl font-bold text-forest mb-4"> Best Tree Service</h1>
+            <p className="text-lg text-bark mb-6">Professional Tree Trimming & Removal in Your Area </p>
+            <button className="bg-forest text-white px-6 py-3 rounded hover:bg-green-700 transition">Get a Free Estimate</button>"
         </section>
     );
 }
