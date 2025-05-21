6f852673b4f444f96c1cdd0c4b1507d30fe37a8a
Joaquin <Mintinvestments95@gmail.com>
2025-05-21 18:19:59 -0500

    removed formdata.name quotes around the curly braces

--- src/pages/EstimateForm.js
+++ src/pages/EstimateForm.js
@@ -22,16 +22,16 @@ export default function EstimateForm() {
             <h2>Get a Free Estimate</h2>
             <form onSubmit={handleSubmit}>
                 <label>Name:
-                <input type="text" name="name" value="{formData.name}" onChange={handleChange} required />
+                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
                  </label>
                  <label>Email:
-                <input type="email" name="email" value="{formData.email}" onChange={handleChange} required />
+                <input type="email" name="email" value={formData.email} onChange={handleChange} required />
                  </label>
                  <label>Phone Number:
-                <input type="tel" name="phone" value="{formData.phone}" onChange={handleChange} required />
+                <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
                     </label>
                     <label>Address:     
-                <input type="text" name="address" value="{formData.address}" onChange={handleChange} required />
+                <input type="text" name="address" value={formData.address} onChange={handleChange} required />
                     </label>
                 <button type="submit">Submit</button>
                 <p>We will contact you shortly to discuss your needs.</p>
