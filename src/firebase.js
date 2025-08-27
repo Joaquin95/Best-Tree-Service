e22d1ec7fa92353b895faf901ccb6ceb29c3391a
Joaquin <Mintinvestments95@gmail.com>
2025-08-27 08:48:20 -0500

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 [
   "REACT_APP_FIREBASE_API_KEY",
@@ -17,22 +18,20 @@ import { getAnalytics } from "firebase/analytics";
 });
 
 const firebaseConfig = {
-  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId:     process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = !getApps().length
-  ? initializeApp(firebaseConfig)
-  : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export const analytics = typeof window !== "undefined"
-  ? getAnalytics(app)
-  : null;
+export const db = getFirestore(app);
+export const analytics =
+  typeof window !== "undefined" ? getAnalytics(app) : null;
 
 const functions = getFunctions(app, "us-central1");
 export const onFormSubmit = httpsCallable(functions, "onFormSubmit");
